be4e502dcc0a3907ca88062efa004cffabda0922
exn <117230748+exxstence@users.noreply.github.com>
2023-06-08 19:56:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -90,12 +90,3 @@ question.forEach(question => {
     }
   })
 })
-
-
-
-kofiWidgetOverlay.draw('mohamedghulam', {
-  'type': 'floating-chat',
-  'floating-chat.donateButton.text': 'Support me',
-  'floating-chat.donateButton.background-color': '#323842',
-  'floating-chat.donateButton.text-color': '#fff'
-});
